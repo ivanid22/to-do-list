82f7d75f8208da026d7e3f3e31e8b368ac75654b
FrancoRosa <km115.franco@gmail.com>
2020-08-14 16:31:09 -0500

    Add render functions for project objects

--- src/index.js
+++ src/index.js
@@ -3,22 +3,68 @@ import Project from './project';
 import Todo from './todo';
 import ChecklistItem from './checklistItem';
 
-const todo1 = Todo('Tarea1');
-const project1 = Project('Proyecto1');
-project1.addTodo(todo1);
+const projects = [];
 
 const eventAggregator = getAggregatorInstance();
 
-eventAggregator.subscribe('addedChecklist', (item) => {
-  console.log('added item');
-  console.log(item);
+
+const renderProjects = () => {
+  let projectContainer = document.querySelector('#projectContainer')
+  if (projectContainer) {
+    projectContainer.innerHTML = '';
+  } else {
+    projectContainer = document.createElement('div');
+    projectContainer.id = 'projectContainer';
+  }
+  projects.forEach(element => {
+    const projectDiv = document.createElement('div');
+    projectDiv.innerHTML = element.getName();
+    projectContainer.appendChild(projectDiv);
+  });
+  
+  projectContainer.appendChild(createNewProject());
+
+  const mainContainer = document.querySelector('#container');
+  mainContainer.innerHTML = '';
+  mainContainer.appendChild(projectContainer);
+};
+
+const validInput = (val) => {
+  return /.+/.test(val);
+};
+
+
+
+eventAggregator.subscribe('createNewProject', (val) => {
+  projects.push(Project(val));
 });
 
-eventAggregator.subscribe('todoUpdated', (data) => {
-  console.log(project1.getTodo(data));
-});
+eventAggregator.subscribe('createNewProject', renderProjects);
 
-setTimeout(() => {
-  todo1.setTitle('new title');
-  todo1.addCheckListItem(ChecklistItem('item'));
-}, 3000);
+
+const createNewProject = () => {
+  const divCreate = document.createElement('div');
+  const itemInput = document.createElement('input');
+  itemInput.setAttribute('type', 'text');
+  itemInput.setAttribute('placeholder', 'New Project');
+  const itemButton = document.createElement('button');
+  itemButton.innerHTML = 'Create';
+  itemButton.onclick = () => {
+    console.log('works?')
+    if (validInput(itemInput.value)) {
+      console.log('Valid')
+      eventAggregator.publish('createNewProject', itemInput.value);
+    }
+  };
+  divCreate.append(itemInput, itemButton);
+  return divCreate;
+};
+
+
+
+
+window.onload = () => {
+  // const mainContainer = document.querySelector('#container');
+  // mainContainer.appendChild(createNewProject());
+  renderProjects();
+};
