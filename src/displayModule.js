f84bb042983030a0b562f01fad4980158b5d3590
Ivan Diaz <ivanid22@gmail.com>
2020-08-17 19:40:45 -0300

    Add display module methods for adding and rendering projects

--- src/displayModule.js
+++ src/displayModule.js
@@ -1,3 +1,44 @@
-(() => {
-  
+import { getAggregatorInstance } from "@ivanid22/js-event-aggregator";
+
+const displayModule = (() => {
+  const eventAggregator = getAggregatorInstance();
+
+  const validInput = (val) => {
+    return /.+/.test(val);
+  };
+
+  const renderProjects = (projects) => {
+    const projectContainer = document.querySelector('.project-container');
+    projectContainer.innerHTML = '';
+    projects.forEach(element => {
+      const projectLi = document.createElement('li');
+      const projectA = document.createElement('a');
+      projectA.textContent = element.getName();
+      projectLi.setAttribute('data-attribute', element.getId());
+      projectLi.appendChild(projectA);
+      projectContainer.appendChild(projectLi);
+    });
+  };
+
+  const initListeners = () => {
+    const itemButton = document.querySelector('.add-project-button');
+    const itemInput = document.querySelector('.add-project-input');
+    itemButton.onclick = () => {
+      if (validInput(itemInput.value)) {
+        itemInput.classList.remove('is-danger');
+        eventAggregator.publish('createNewProject', itemInput.value);
+        itemInput.value = '';
+      }
+      else {
+        itemInput.classList.add('is-danger');
+      }
+    };
+  };
+
+  return {
+    initListeners,
+    renderProjects,
+  };
 })();
+
+export default displayModule;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './css/style.css'
 import { getAggregatorInstance } from '@ivanid22/js-event-aggregator';
 import Project from './project';
 import moment from 'moment';
+import displayModule from './displayModule';
 import Todo from './todo';
 import ChecklistItem from './checklistItem';
 
@@ -12,65 +13,12 @@ const eventAggregator = getAggregatorInstance();
 
 window.moment = moment;
 
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
-
-const renderProjects = () => {
-  let projectContainer = document.querySelector('#projectContainer')
-  if (projectContainer) {
-    projectContainer.innerHTML = '';
-  } else {
-    projectContainer = document.createElement('div');
-    projectContainer.id = 'projectContainer';
-  }
-  projects.forEach(element => {
-    const projectDiv = document.createElement('div');
-    projectDiv.innerHTML = element.getName();
-    projectContainer.appendChild(projectDiv);
-  });
-  
-  projectContainer.appendChild(createNewProject());
-
-  const mainContainer = document.querySelector('#container');
-  mainContainer.innerHTML = '';
-  mainContainer.appendChild(projectContainer);
-};
-
-const validInput = (val) => {
-  return /.+/.test(val);
-};
-
-
-
 eventAggregator.subscribe('createNewProject', (val) => {
   projects.push(Project(val));
+  displayModule.renderProjects(projects);
 });
 
-eventAggregator.subscribe('createNewProject', renderProjects);
-
-
-const createNewProject = () => {
-  const divCreate = document.createElement('div');
-  const itemInput = document.createElement('input');
-  itemInput.setAttribute('type', 'text');
-  itemInput.setAttribute('placeholder', 'New Project');
-  const itemButton = document.createElement('button');
-  itemButton.innerHTML = 'Create';
-  itemButton.onclick = () => {
-    console.log('works?')
-    if (validInput(itemInput.value)) {
-      console.log('Valid')
-      eventAggregator.publish('createNewProject', itemInput.value);
-    }
-  };
-  divCreate.append(itemInput, itemButton);
-  return divCreate;
-};
-
-
-
-
 window.onload = () => {
-  // const mainContainer = document.querySelector('#container');
-  // mainContainer.appendChild(createNewProject());
-  renderProjects();
+  displayModule.renderProjects(projects);
+  displayModule.initListeners();
 };
